6a5914ef56be6368d338dccdc48effabeefc2c2f
nathanial poor <wownatepoor@hotmail.com>
2023-08-02 16:24:27 -0700

    added more java topics

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+console.log(topics[0]);
+console.log(topics[1]);
+console.log(topics[2]);
+console.log(topics[3]);
